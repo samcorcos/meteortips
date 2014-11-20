5c3541579610b4ef3ed59af978496e54d6f422e1
Sam Corcos <samcorcos@gmail.com>
2014-11-20 00:12:23 +0000

    set session variable for selectedPlayer

--- leaderboard.js
+++ leaderboard.js
@@ -14,15 +14,18 @@ if(Meteor.isClient){
   
   Template.leaderboard.events({
     "click .player": function(){
-      console.log("You clicked on a player element");
+      var playerId = this._id;
+      Session.set("selectedPlayer", playerId);
     }
   })
   
+  // Sessions are used to store small pieces of data that aren't saved to the database and won't be remembered on return visits. This sort of data might not sound useful, but it is. 
+  // Session.set takes in two arguments. 
+  // First, passing through a name for teh session. We're calling our session "selectedPlayer"
+  // Second, we're passing through a value for the session. This is the data stored in the session itself. 
   
-  
-  
-  
-  
+  // We're passing in "this", which depends on it's context. In this context, "this" refers to the player that has been clicked
+  // The underscore in the _id doesn't have any special significance. It's just a way of showing that it's the mongodb identifier.
   
 }
 
